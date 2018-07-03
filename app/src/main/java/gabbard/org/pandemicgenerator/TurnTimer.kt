053047e155ef8cccaf062c0109648fcd3cd8279d
Ryan Gabbard <gabbard@isi.edu>
2018-07-02 22:08:00 -0400

    Turn timer works
    Closes #12

--- app/src/main/java/gabbard/org/pandemicgenerator/TurnTimer.kt
+++ app/src/main/java/gabbard/org/pandemicgenerator/TurnTimer.kt
@@ -1,12 +1,16 @@
 package gabbard.org.pandemicgenerator
 
 import android.content.Intent
+import android.media.RingtoneManager
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Chronometer
 import kotlinx.android.synthetic.main.activity_turn_timer.*
 import org.gabbard.pandemicgenerator.TrackableState
 import java.util.*
+
 
 class TurnTimer : AppCompatActivity() {
     private var gameState: TrackableState? = null
@@ -20,9 +24,23 @@ class TurnTimer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_turn_timer)
-        timeRemaining.text = "1:15"
         gameState = intent.getSerializableExtra(GAME_STATE) as TrackableState
         rng = intent.getSerializableExtra(RANDOM_SOURCE) as Random
+        timeRemaining.isCountDown = true
+        timeRemaining.start()
+        val targetTime = SystemClock.elapsedRealtime() + 75 * 1000
+        timeRemaining.base = targetTime
+        timeRemaining.onChronometerTickListener = Chronometer.OnChronometerTickListener {
+            if (SystemClock.elapsedRealtime() >= targetTime) {
+                try {
+                    val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                    val r = RingtoneManager.getRingtone(applicationContext, notification)
+                    r.play()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 
     fun onDrawPlayerCards(view: View) {
