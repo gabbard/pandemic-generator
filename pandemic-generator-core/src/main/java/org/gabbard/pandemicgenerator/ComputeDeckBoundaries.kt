a4b1d47d9ca9fbd3504b3c93ea9983ecedf315db
Ryan Gabbard <gabbard@isi.edu>
2020-08-06 14:46:18 -0400

    Snapshot

--- pandemic-generator-core/src/main/java/org/gabbard/pandemicgenerator/ComputeDeckBoundaries.kt
+++ pandemic-generator-core/src/main/java/org/gabbard/pandemicgenerator/ComputeDeckBoundaries.kt
@@ -12,25 +12,32 @@ fun main(args: Array<String>) {
     val rng = Random(0)
 
     val ruleSet = NATIONAL_CHAMPIONSHIP_RULES
+    // epidemics are not accounted for in the deck for making player hands, so we offset
+    // the number of cards remove for player hands by the number of epidemic to balance it out
+    // this result in stacks with different content than the real game, but the same boundaries
+    val numCardsToRemoveBeforeStacks = NUM_CARDS_FOR_PLAYER_HANDS - ruleSet.numEpidemicsToUse
     val deckAfterPlayerHands =
-            ruleSet.createDeckForPlayerHands(rng).draw(NUM_CARDS_FOR_PLAYER_HANDS).second
+            ruleSet.createDeckForPlayerHands(rng).draw(numCardsToRemoveBeforeStacks).second
     val stacks =
             deckAfterPlayerHands.splitAsEvenlyAsPossible(ruleSet.numEpidemicsToUse).toMutableList()
 
     val stackIndexToFirstCardIndex =
             MutableList(ruleSet.numEpidemicsToUse) { _ -> HashMultiset.create<Int>() }
 
+    // two cards are drawn each turn. turns are 1-indexed for non-CS users :-)
+    fun turnForCard(cardNum: Int) = cardNum / 2 + 1
+
     (1..numSamples).forEach {
         stacks.shuffle(rng)
         var cardsSoFar = 0
         stacks.forEachIndexed { index, stack ->
-            stackIndexToFirstCardIndex[index].add(cardsSoFar)
+            stackIndexToFirstCardIndex[index].add(turnForCard(cardsSoFar))
             cardsSoFar += stack.size
         }
     }
 
     stackIndexToFirstCardIndex.withIndex().forEach {
-        print("\nStack ${it.index}: " +
+        print("\nStack ${it.index + 1}: " +
                 it.value.entrySet().sortedBy { it.element }
                         .joinToString(", ") {
                             "${it.element} = ${it.count / numSamples.toDouble()}"
